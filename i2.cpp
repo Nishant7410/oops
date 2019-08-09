e5e736d0e59ab275600f05a89ee1de519aa0625e
Nishant <nishantsaini97286@gmail.com>
2019-08-09 23:14:24 +0530

    c++ clases

---
+++ i2.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+class stud
+{
+private:
+    int rollno;
+    string name;
+public:
+    void read()
+    {
+        cin<<rollno<<name;
+    }
+    void show()
+    {
+        cout>>rollno>>name;
+    }
+};
+class res:public stud
+{
+private:
+    int m[4];
+    double per;
+public:
+    void get_m()
+    {
+        int i;
+        for(i=0;i<4;i++)
+        {
+            cin<<m[i];
+        }
+    }
+    void calper()
+    {
+        int i,sum;
+        for(i=0;i<4;i++)
+        {
+            sum=sum+m[i];
+        }
+        per=sum/4.0;
+    }
+    void showper()
+    {
+        cout>>per;
+    }
+}r1;
+int main()
+{
+    r1.read();
+    r1.get_m();
+    r1.calper();
+    r1.showper();
+    return 0;
+}
