4d853c5fedd2a474b4105594954222ad1bf07e17
Nishant <nishantsaini97286@gmail.com>
2019-08-22 23:54:07 +0530

    rat in maze

--- rat in a maze.cpp
+++ rat in a maze.cpp
@@ -1,35 +1,37 @@
 #include<iostream>
 using namespace std;
-void fun(int ar[100][100],int i,int j,int n)
+void fun(int ar[100][100],int i,int j,int n,char *ch,int k)
 {
     if(i<0||j<0||i>=n||j>=n)
         return;
     else if(i==n-1&&j==n-1&&ar[i][j]==1)
     {
+        ch[k]='\0';
+        cout<<ch;
         return;
     }
     else if(ar[i][j]==0||ar[i][j]==-1)
         return;
     ar[i][j]=-1;
-    fun(ar,i,j+1,n);
-    fun(ar,i+1,j,n);
-    fun(ar,i-1,j,n);
-    fun(ar,i,j-1,n);
+    ch[k]='R';
+    fun(ar,i,j+1,n,ch,k+1);
+    ch[k]='D';
+    fun(ar,i+1,j,n,ch,k+1);
+    ch[k]='L';
+    fun(ar,i,j-1,n,ch,k+1);
+    ch[k]='U';
+    fun(ar,i-1,j,n,ch,k+1);
     ar[i][j]=1;
 }
 int main()
 {
     int n;
     cin>>n;
-    int ar[n][n];
+    int ar[100][100];
     for(int i=0;i<n;i++)
     for(int j=0;j<n;j++)
     cin>>ar[i][j];
-    fun(ar,0,0,n);
-    for(int i=0;i<n;i++)
-    {
-    for(int j=0;j<n;j++)
-    cout<<ar[i][j]<<" ";
-    cout<<endl;
-    }
+    char *ch=new char[n+n];
+    fun(ar,0,0,n,ch,0);
+    //cout<<ch;
 }
