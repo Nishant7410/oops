8028ea5d93826fb39c5ac8968438168e3b941c31
Nishant <nishantsaini97286@gmail.com>
2019-10-05 16:26:13 +0530

    find min element in stack in o(1)

---
+++ getMinInStack.cpp
@@ -0,0 +1,83 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+struct MyStack
+{
+    stack<int> s;
+    int minEle;
+    void getMin()
+    {
+        if (s.empty())
+            cout << "Stack is empty\n";
+        else
+            cout <<"Minimum Element in the stack is: "
+                 << minEle << "\n";
+    }
+    void peek()
+    {
+        if (s.empty())
+        {
+            cout << "Stack is empty ";
+            return;
+        }
+        int t = s.top();
+        cout << "Top Most Element is: ";
+        (t < minEle)? cout << minEle: cout << t;
+    }
+    void pop()
+    {
+        if (s.empty())
+        {
+            cout << "Stack is empty\n";
+            return;
+        }
+
+        cout << "Top Most Element Removed: ";
+        int t = s.top();
+        s.pop();
+        if (t < minEle)
+        {
+            cout << minEle << "\n";
+            minEle = 2*minEle - t;
+        }
+
+        else
+            cout << t << "\n";
+    }
+    void push(int x)
+    {
+        if (s.empty())
+        {
+            minEle = x;
+            s.push(x);
+            cout <<  "Number Inserted: " << x << "\n";
+            return;
+        }
+        if (x < minEle)
+        {
+            s.push(2*x - minEle);
+            minEle = x;
+        }
+
+        else
+           s.push(x);
+
+        cout <<  "Number Inserted: " << x << "\n";
+    }
+};
+int main()
+{
+    MyStack s;
+    s.push(8);
+    s.push(6);
+    s.getMin();
+    s.push(7);
+    s.push(2);
+    s.getMin();
+    s.pop();
+    s.getMin();
+    s.pop();
+    s.peek();
+
+    return 0;
+}
