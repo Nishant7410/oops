9cb5654db7a532b8be2bf45c3012fe59e5b3d876
Nishant <nishantsaini97286@gmail.com>
2019-12-04 21:08:44 +0530

    rain water problem

---
+++ rain water trapping.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    int water=0;
+    for(int i=0;i<n;i++)
+        cin>>arr[i];
+    int left[n],right[n];
+    left[0]=arr[0];
+    for(int i=1;i<n;i++)
+        left[i]=max(left[i-1],arr[i]);
+    right[n-1]=arr[n-1];
+    for(int i=n-2;i>=0;i--)
+        right[i]=max(right[i+1],arr[i]);
+    for(int i=0;i<n;i++)
+    water+=min(left[i],right[i])-arr[i];
+    cout<<water;
+}
